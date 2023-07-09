8804a00b3d12a5e6dfaaebe79e7d61dccdd0027b
BerkkanB <butun18@itu.edu.tr>
2023-07-09 16:17:27 +0300

    Source screen and viewmodel added with response updates

--- app/src/main/java/com/berkkanb/boyner/presentation/source/SourceScreen.kt
+++ app/src/main/java/com/berkkanb/boyner/presentation/source/SourceScreen.kt
@@ -1,9 +1,29 @@
 package com.berkkanb.boyner.presentation.source
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.berkkanb.boyner.presentation.coreui.LoadingScreen
+import com.berkkanb.boyner.presentation.source.components.NewsSourceCard
 
 @Composable
-fun SourceScreen() {
-    Text(text = "Source")
+fun SourceScreen(
+    sourceScreenViewModel: SourceScreenViewModel = hiltViewModel()
+) {
+    val uiState by sourceScreenViewModel.uiState.collectAsState()
+
+    if (uiState.isLoading){
+        LoadingScreen()
+    } else {
+        LazyColumn(){
+            uiState.sourceList?.let { sourceList ->
+                items(sourceList, key = { it.sourceId }){
+                    NewsSourceCard(title = it.sourceName, description = it.sourceDescription)
+                }
+            }
+        }
+    }
 }
